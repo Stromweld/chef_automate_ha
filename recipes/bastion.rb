892c8aa39cbe2485ffc2178a467328f273b3c1fe
Corey Hemminger <hemminger@hotmail.com>
2022-11-28 15:36:02 -0600

    initial commit
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

---
+++ recipes/bastion.rb
@@ -0,0 +1,91 @@
+#
+# Cookbook:: automate_ha
+# Recipe:: bastion
+#
+# Copyright:: 2022, Corey Hemminger
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Create automate_ha user ssh private key in root's .ssh folder
+file "#{Chef::Config[:file_cache_path]}/automate_ha.key" do
+  content node['automate_ha']['ssh_key']
+  owner 'root'
+  group 'root'
+  mode '0600'
+  sensitive true
+end
+
+bin_path = value_for_platform_family(
+  debian: '/bin',
+  default: '/usr/bin'
+)
+
+remote_file "#{Chef::Config[:file_cache_path]}/chef-automate-#{node['automate_ha']['version']}.zip" do
+  source "https://packages.chef.io/files/current/#{node['automate_ha']['version']}/chef-automate-cli/chef-automate_linux_amd64.zip"
+end
+
+archive_file "#{Chef::Config[:file_cache_path]}/chef-automate-#{node['automate_ha']['version']}.zip" do
+  destination '/usr/local/chef-automate'
+  mode '0755'
+end
+
+link "#{bin_path}/chef-automate" do
+  to '/usr/local/chef-automate/chef-automate'
+end
+
+remote_file "#{Chef::Config[:file_cache_path]}/automate-#{node['automate_ha']['version']}.aib" do
+  source "https://packages.chef.io/airgap_bundle/current/automate/#{node['automate_ha']['version']}.aib"
+end
+
+sysctl 'vm.max_map_count' do
+  value 262144
+end
+
+sysctl 'vm.dirty_expire_centisecs' do
+  value 20000
+end
+
+template "#{Chef::Config[:file_cache_path]}/config.toml" do
+  source 'config.toml.erb'
+  variables(var: render_toml(node['automate_ha']['initial_config_toml_template']))
+end
+
+execute "chef-automate deploy #{Chef::Config[:file_cache_path]}/config.toml --airgap-bundle #{Chef::Config[:file_cache_path]}/automate-#{node['automate_ha']['version']}.aib #{'--accept-terms-and-mlsa' if node['automate_ha']['accept_license']}" do
+  cwd Chef::Config[:file_cache_path]
+  live_stream true
+  user 'root'
+  timeout 7200
+  not_if { shell_out("#{bin_path}/chef-automate service-versions").exitstatus.eql?(0) }
+  notifies :run, 'execute[chef-automate status]', :delayed
+end
+
+execute "chef-automate status" do
+  cwd Chef::Config[:file_cache_path]
+  live_stream true
+  user 'root'
+  action :nothing
+end
+
+if node['automate_ha']['patch_config_toml_template']
+  template "#{Chef::Config[:file_cache_path]}/patch_config.toml" do
+    source 'patch_config.toml.erb'
+    variables(var: render_toml(node['automate_ha']['patch_config_toml_template']))
+    notifies :run, "execute[chef-automate config patch #{Chef::Config[:file_cache_path]}/patch_config.toml]", :immediately
+  end
+
+  execute "chef-automate config patch #{Chef::Config[:file_cache_path]}/patch_config.toml" do
+    cwd Chef::Config[:file_cache_path]
+    live_stream true
+    action :nothing
+  end
+end
---
+++ recipes/default.rb
@@ -0,0 +1,74 @@
+#
+# Cookbook:: automate_ha
+# Recipe:: default
+#
+# Copyright:: 2022, Corey Hemminger
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Create common user for all nodes
+user_name = node['automate_ha']['username']
+
+user user_name
+
+# Gather any existing authorized_keys entries if they exist
+if ::File.exist?("/home/#{user_name}/.ssh/authorized_keys")
+  pub_key = ::File.read("/home/#{user_name}/.ssh/authorized_keys").split.max_by(&:length)
+  node.force_override['automate_ha']['ssh_authorize_key'] = node['automate_ha']['ssh_authorize_key'].merge(
+    {
+      "#{user_name}" => {
+        key: pub_key,
+        user: user_name,
+        options: nil,
+      },
+    }
+  )
+end
+
+# Create Authorized Keys entries
+node['automate_ha']['ssh_authorize_key']&.each do |name, hash|
+  ssh_authorize_key name do
+    hash&.each do |key, value|
+      send(key, value)
+    end
+  end
+end
+
+# Ensure user has sudo permissions without password
+sudo node['automate_ha']['username'] do
+  nopasswd true
+  users node['automate_ha']['username']
+end
+
+# Create /etc/hosts file entries if not using DNS
+unless node['automate_ha']['dns_configured']
+  replace_or_add 'automate' do
+    path '/etc/hosts'
+    pattern ".* #{node['automate_ha']['automate_dns_entry']}"
+    line "#{node['automate_ha']['instance_ips']['automate_frontend'].first} #{node['automate_ha']['automate_dns_entry']}"
+    remove_duplicates true
+  end
+
+  replace_or_add 'infra-server' do
+    path '/etc/hosts'
+    pattern ".* #{node['automate_ha']['infra-server_dns_entry']}"
+    line "#{node['automate_ha']['instance_ips']['chef_frontend'].first} #{node['automate_ha']['infra-server_dns_entry']}"
+    remove_duplicates true
+  end
+end
+
+# Set SElinux to permissive mode
+selinux_state 'default' do
+  # automatic_reboot true
+  action :permissive
+end
