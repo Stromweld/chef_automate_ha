a5ac57fd8ad0a1995b8acb4e2b2a914d8d3d4d54
Corey Hemminger <hemminger@hotmail.com>
2022-12-05 13:15:18 -0600

    bugfix in config.toml and dns attributes, other perf tweaks
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- start_kitchen_test.sh
+++ start_kitchen_test.sh
@@ -13,13 +13,13 @@ platform="${1:-almalinux-8}"
 
 # Bring up kitchen nodes
 echo "**** Starting kitchen create $platform ****"
-kitchen create $platform
+kitchen create $platform -c 3
 
 echo "**** Writing IP's of machines to kitchen_nodes.json ****"
 if [[ "$KITCHEN_LOCAL_YAML" == "kitchen.ec2.yml" ]]
   then
-    chef_frontend="$(aws --profile saml ec2 describe-instances --instance-ids $(cat .kitchen/chef-frontend-$platform.yml | grep server_id | cut -d ' ' -f 2) --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text --region us-west-2)"
-    automate_frontend="$(aws --profile saml ec2 describe-instances --instance-ids $(cat .kitchen/automate-frontend-$platform.yml | grep server_id | cut -d ' ' -f 2) --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text --region us-west-2)"
+    chef_frontend_1="$(aws --profile saml ec2 describe-instances --instance-ids $(cat .kitchen/chef-frontend-1-$platform.yml | grep server_id | cut -d ' ' -f 2) --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text --region us-west-2)"
+    automate_frontend_1="$(aws --profile saml ec2 describe-instances --instance-ids $(cat .kitchen/automate-frontend-1-$platform.yml | grep server_id | cut -d ' ' -f 2) --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text --region us-west-2)"
     postgres_backend_1="$(aws --profile saml ec2 describe-instances --instance-ids $(cat .kitchen/postgres-backend-1-$platform.yml | grep server_id | cut -d ' ' -f 2) --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text --region us-west-2)"
     postgres_backend_2="$(aws --profile saml ec2 describe-instances --instance-ids $(cat .kitchen/postgres-backend-2-$platform.yml | grep server_id | cut -d ' ' -f 2) --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text --region us-west-2)"
     postgres_backend_3="$(aws --profile saml ec2 describe-instances --instance-ids $(cat .kitchen/postgres-backend-3-$platform.yml | grep server_id | cut -d ' ' -f 2) --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text --region us-west-2)"
@@ -30,20 +30,20 @@ if [[ "$KITCHEN_LOCAL_YAML" == "kitchen.ec2.yml" ]]
 elif [[ "$KITCHEN_LOCAL_YAML" == "kitchen.dokken.yml" ]]
   then
     # Get external IPs
-    chef_frontend="$(docker network inspect dokken -f '{{json .Containers}}' | jq '.[] | .Name + ":" + .IPv4Address' | grep chef-frontend | cut -d ':' -f2 | cut -d '/' -f1)"
-    automate_frontend="$(docker network inspect dokken -f '{{json .Containers}}' | jq '.[] | .Name + ":" + .IPv4Address' | grep automate-frontend | cut -d ':' -f2 | cut -d '/' -f1)"
-    postgres_backend_1="$(docker network inspect dokken -f '{{json .Containers}}' | jq '.[] | .Name + ":" + .IPv4Address' | grep postgres-backend-1 | cut -d ':' -f2 | cut -d '/' -f1)"
-    postgres_backend_2="$(docker network inspect dokken -f '{{json .Containers}}' | jq '.[] | .Name + ":" + .IPv4Address' | grep postgres-backend-2 | cut -d ':' -f2 | cut -d '/' -f1)"
-    postgres_backend_3="$(docker network inspect dokken -f '{{json .Containers}}' | jq '.[] | .Name + ":" + .IPv4Address' | grep postgres-backend-3 | cut -d ':' -f2 | cut -d '/' -f1)"
-    opensearch_backend_1="$(docker network inspect dokken -f '{{json .Containers}}' | jq '.[] | .Name + ":" + .IPv4Address' | grep opensearch-backend-1 | cut -d ':' -f2 | cut -d '/' -f1)"
-    opensearch_backend_2="$(docker network inspect dokken -f '{{json .Containers}}' | jq '.[] | .Name + ":" + .IPv4Address' | grep opensearch-backend-2 | cut -d ':' -f2 | cut -d '/' -f1)"
-    opensearch_backend_3="$(docker network inspect dokken -f '{{json .Containers}}' | jq '.[] | .Name + ":" + .IPv4Address' | grep opensearch-backend-3 | cut -d ':' -f2 | cut -d '/' -f1)"
-    bastion="$(docker network inspect dokken -f '{{json .Containers}}' | jq '.[] | .Name + ":" + .IPv4Address' | grep bastion | cut -d ':' -f2 | cut -d '/' -f1)"
+    chef_frontend_1="$(docker network inspect dokken -f '{{json .Containers}}' | jq '.[] | .Name + ":" + .IPv4Address' | grep chef-frontend-1-$platform | cut -d ':' -f2 | cut -d '/' -f1)"
+    automate_frontend_1="$(docker network inspect dokken -f '{{json .Containers}}' | jq '.[] | .Name + ":" + .IPv4Address' | grep automate-frontend-1-$platform | cut -d ':' -f2 | cut -d '/' -f1)"
+    postgres_backend_1="$(docker network inspect dokken -f '{{json .Containers}}' | jq '.[] | .Name + ":" + .IPv4Address' | grep postgres-backend-1-$platform | cut -d ':' -f2 | cut -d '/' -f1)"
+    postgres_backend_2="$(docker network inspect dokken -f '{{json .Containers}}' | jq '.[] | .Name + ":" + .IPv4Address' | grep postgres-backend-2-$platform | cut -d ':' -f2 | cut -d '/' -f1)"
+    postgres_backend_3="$(docker network inspect dokken -f '{{json .Containers}}' | jq '.[] | .Name + ":" + .IPv4Address' | grep postgres-backend-3-$platform | cut -d ':' -f2 | cut -d '/' -f1)"
+    opensearch_backend_1="$(docker network inspect dokken -f '{{json .Containers}}' | jq '.[] | .Name + ":" + .IPv4Address' | grep opensearch-backend-1-$platform | cut -d ':' -f2 | cut -d '/' -f1)"
+    opensearch_backend_2="$(docker network inspect dokken -f '{{json .Containers}}' | jq '.[] | .Name + ":" + .IPv4Address' | grep opensearch-backend-2-$platform | cut -d ':' -f2 | cut -d '/' -f1)"
+    opensearch_backend_3="$(docker network inspect dokken -f '{{json .Containers}}' | jq '.[] | .Name + ":" + .IPv4Address' | grep opensearch-backend-3-$platform | cut -d ':' -f2 | cut -d '/' -f1)"
+    bastion="$(docker network inspect dokken -f '{{json .Containers}}' | jq '.[] | .Name + ":" + .IPv4Address' | grep bastion-$platform | cut -d ':' -f2 | cut -d '/' -f1)"
 elif [[ "$VAGRANT_DEFAULT_PROVIDER" == "parallels" ]]
   then
     # Get external IPs
-    chef_frontend="$(prlctl exec $(cat .kitchen/kitchen-vagrant/chef-frontend-$platform/.vagrant/machines/default/parallels/id) ip -o -f inet address show eth0 | cut -d ' ' -f7 | cut -d '/' -f1)"
-    automate_frontend="$(prlctl exec $(cat .kitchen/kitchen-vagrant/automate-frontend-$platform/.vagrant/machines/default/parallels/id) ip -o -f inet address show eth0 | cut -d ' ' -f7 | cut -d '/' -f1)"
+    chef_frontend_1="$(prlctl exec $(cat .kitchen/kitchen-vagrant/chef-frontend-1-$platform/.vagrant/machines/default/parallels/id) ip -o -f inet address show eth0 | cut -d ' ' -f7 | cut -d '/' -f1)"
+    automate_frontend_1="$(prlctl exec $(cat .kitchen/kitchen-vagrant/automate-frontend-1-$platform/.vagrant/machines/default/parallels/id) ip -o -f inet address show eth0 | cut -d ' ' -f7 | cut -d '/' -f1)"
     postgres_backend_1="$(prlctl exec $(cat .kitchen/kitchen-vagrant/postgres-backend-1-$platform/.vagrant/machines/default/parallels/id) ip -o -f inet address show eth0 | cut -d ' ' -f7 | cut -d '/' -f1)"
     postgres_backend_2="$(prlctl exec $(cat .kitchen/kitchen-vagrant/postgres-backend-2-$platform/.vagrant/machines/default/parallels/id) ip -o -f inet address show eth0 | cut -d ' ' -f7 | cut -d '/' -f1)"
     postgres_backend_3="$(prlctl exec $(cat .kitchen/kitchen-vagrant/postgres-backend-3-$platform/.vagrant/machines/default/parallels/id) ip -o -f inet address show eth0 | cut -d ' ' -f7 | cut -d '/' -f1)"
@@ -53,8 +53,8 @@ elif [[ "$VAGRANT_DEFAULT_PROVIDER" == "parallels" ]]
     bastion="$(prlctl exec $(cat .kitchen/kitchen-vagrant/bastion-$platform/.vagrant/machines/default/parallels/id) ip -o -f inet address show eth0 | cut -d ' ' -f7 | cut -d '/' -f1)"
 else
   # Get external IPs
-  chef_frontend="$(VBoxManage guestproperty get $(cat .kitchen/kitchen-vagrant/chef-frontend-$platform/.vagrant/machines/default/virtualbox/id) "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{print $2}')"
-  automate_frontend="$(VBoxManage guestproperty get $(cat .kitchen/kitchen-vagrant/automate-frontend-$platform/.vagrant/machines/default/virtualbox/id) "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{print $2}')"
+  chef_frontend_1="$(VBoxManage guestproperty get $(cat .kitchen/kitchen-vagrant/chef-frontend-1-$platform/.vagrant/machines/default/virtualbox/id) "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{print $2}')"
+  automate_frontend_1="$(VBoxManage guestproperty get $(cat .kitchen/kitchen-vagrant/automate-frontend-1-$platform/.vagrant/machines/default/virtualbox/id) "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{print $2}')"
   postgres_backend_1="$(VBoxManage guestproperty get $(cat .kitchen/kitchen-vagrant/postgres-backend-1-$platform/.vagrant/machines/default/virtualbox/id) "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{print $2}')"
   postgres_backend_2="$(VBoxManage guestproperty get $(cat .kitchen/kitchen-vagrant/postgres-backend-2-$platform/.vagrant/machines/default/virtualbox/id) "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{print $2}')"
   postgres_backend_3="$(VBoxManage guestproperty get $(cat .kitchen/kitchen-vagrant/postgres-backend-3-$platform/.vagrant/machines/default/virtualbox/id) "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{print $2}')"
@@ -68,8 +68,8 @@ fi
 cat <<-EOF | tee kitchen_nodes.json
   {
     "bastion": ["$bastion"],
-    "chef_frontend": ["$chef_frontend"],
-    "automate_frontend": ["$automate_frontend"],
+    "chef_frontend": ["$chef_frontend_1"],
+    "automate_frontend": ["$automate_frontend_1"],
     "postgres_backend": ["$postgres_backend_1", "$postgres_backend_2", "$postgres_backend_3"],
     "opensearch_backend": ["$opensearch_backend_1", "$opensearch_backend_2", "$opensearch_backend_3"]
   }
@@ -77,6 +77,7 @@ EOF
 
 # Converge kitchen nodes
 echo "**** Starting kitchen converge $platform ****"
-kitchen converge $platform
+kitchen converge end-[0-9]-$platform
+kitchen converge bastion-$platform
 
 echo "**** Script complete ****"
